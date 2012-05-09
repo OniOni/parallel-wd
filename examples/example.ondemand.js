198a615f7279bb7ad0966eb2b1f4a37c01b79ecb
msabourin <mathieu.c.sabourin@gmail.com>
2012-05-09 15:34:10 -0700

    An example of wd-parallel in action.

--- examples/example.ondemand.js
+++ examples/example.ondemand.js
@@ -1,7 +1,7 @@
-// CONFIGURE SAUCE CREDENTIALS HER!E
-var username = "msabourin",
-accessKey = "aae81464-8dd5-41b4-93e6-2c8426bbf5d6";
-  
+// CONFIGURE SAUCE CREDENTIALS HERE !
+var username = "<USERNAME>",
+accessKey = "<ACCESS KEY>";
+
 var p_webdriver;
 try {
   p_webdriver = require('wd-parallel');
@@ -9,52 +9,75 @@ try {
   p_webdriver = require('../lib/main');
 }
 
-var assert = require('assert');
+var browsers = p_webdriver.remote(
+    "ondemand.saucelabs.com", 
+    80,
+    username, 
+    accessKey
+);
 
-var browsers = p_webdriver.remote("ondemand.saucelabs.com", 80, username, accessKey);
-
-var desired = [
+var p_desired = [
     {
 	tags: ["examples"]
-	, name: "This is an example test"
+	, name: "parallel test 1/4"
 	, browserName: "firefox"
 	, javascriptEnabled: true
-	, command: function(meth, path){
-	    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
-	}
-	, status: function(info){
-	    console.log('\x1b[36m%s\x1b[0m', info);
+	, callbacks: 
+	{
+	    command: function(meth, path){
+		console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
+	    }
+	    , status: function(info){
+		console.log('\x1b[36m%s\x1b[0m', info);
+	    }
 	}
     }
     ,
     {
 	tags: ["examples"]
-	, name: "This is an example test"
+	, name: "parallel test 2/4"
 	, browserName: "chrome"
 	, javascriptEnabled: true
-	, command: function(meth, path){
-	    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
-	}
-	, status: function(info){
-	    console.log('\x1b[36m%s\x1b[0m', info);
-	}
+    }
+    ,
+    {
+	tags: ["examples"]
+	, name: "parallel test 3/4"
+	, browserName: "firefox"
+	, platform: "LINUX"
+	, javascriptEnabled: true
+    }
+    ,
+    {
+	tags: ["examples"]
+	, name: "parallel test 4/4"
+	, browserName: "chrome"
+	, platform: "LINUX"
+	, javascriptEnabled: true
     }
 ]
 
-browsers.init(desired, function() {
-  browsers.get("http://saucelabs.com/test/guinea-pig", function() {
-    browsers.title(function(err, title) {
-      assert.ok(~title.indexOf('I am a page title - Sauce Labs'), 'Wrong title!');
-      browsers.elementById('submit', function(err, el) {
-        browsers.clickElement(el, function() {
-          browsers.eval("window.location.href", function(err, title) {
-            assert.ok(~title.indexOf('#'), 'Wrong title!');
-            browsers.quit()
-          })
-        })
-      })
-    })
-  })
-})
+browsers.test = function(browser, desired) {
+
+    console.log("server status:", browser.status());
+    browser.init(desired);
+        
+    browser.get("http://google.com");
+    console.log("title is "+browser.title());
+
+    var queryField = browser.elementByName('q');
+    browser.type(queryField, "Hello World");
+    browser.type(queryField, "\n");
+
+    browser.setWaitTimeout(3000);
+    browser.elementByCss('#ires'); // waiting for new page to load
+    console.log(browser.title());
+
+    browser.quit();
+
+};
+
+browsers.run(p_desired);
+
 
 
